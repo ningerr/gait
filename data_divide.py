e2957309678e2be423ea8c390f399826be63ccf6
ningerr <2638179771@qq.com>
2025-01-07 15:32:06 +0800

    sample expend

--- data_divide.py
+++ data_divide.py
@@ -1,14 +1,7 @@
 """
 数据集划分
 分为训练集和测试集
-针对 CASIA_B_2020数据集
-rootpath--|000-|1-|1.txt
-                 -|2.txt
-                 ...
-              -|2
-              ...
-        --|018
-          ...
+
 """
 import os
 import random
@@ -18,10 +11,10 @@ import numpy as np
 
 
 def save_data(file_path, save_dir, order_index):
-
-    name = str(order_index)+'.txt'
-    output_dir = os.path.join(save_dir,name)
+    name = str(order_index) + '.txt'
+    output_dir = os.path.join(save_dir, name)
     shutil.copy(file_path, output_dir)
+
 
 def select_lable(path):
     selected = []
@@ -46,6 +39,18 @@ def select_lable(path):
 
 
 def dvd2(path):
+    """
+    针对 CASIA_B_2020数据集
+    path--|000-|1-|1.txt
+                 -|2.txt
+                 ...
+              -|2
+              ...
+        --|018
+          ...
+    :param path:
+    :return:
+    """
     angles = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
     valid_count = 0
     test_count = 0
@@ -64,17 +69,17 @@ def dvd2(path):
                     datas = os.listdir(people_path)
                     # 4：1：1划分并保存
                     random.shuffle(datas)
-                    train_data = datas[:len(datas)*4//6]
-                    valid_data = datas[len(datas)*4//6:len(datas)*4//6+len(datas)//6]
-                    test_data = datas[len(datas)*4//6+len(datas)//6:]
+                    train_data = datas[:len(datas) * 4 // 6]
+                    valid_data = datas[len(datas) * 4 // 6:len(datas) * 4 // 6 + len(datas) // 6]
+                    test_data = datas[len(datas) * 4 // 6 + len(datas) // 6:]
                     for data in train_data:
-                        train_path = 'E:\\Dataset\\CASIB_dvd\\train\\'+person+'\\'
+                        train_path = 'E:\\Dataset\\CASIB_dvd\\train\\' + person + '\\'
                         data_path = os.path.join(people_path, data)
                         os.makedirs(train_path, exist_ok=True)
                         train_count[person] += 1
                         save_data(data_path, train_path, train_count[person])
                     for data in test_data:
-                        test_path = 'E:\\Dataset\\CASIB_dvd\\test\\'+person+'\\'
+                        test_path = 'E:\\Dataset\\CASIB_dvd\\test\\' + person + '\\'
                         data_path = os.path.join(people_path, data)
                         os.makedirs(test_path, exist_ok=True)
                         test_count[person] += 1
@@ -87,12 +92,56 @@ def dvd2(path):
                     datas = os.listdir(people_path)
 
 
+def dvd3(inpath, outpath):
+    """
+    ev-casia-A
+    inpath--name1--00_1_1.txt
+                 --00_1_2.txt
+                 --45_1_1.txt
+          --name2--……
+
+    :param inpath:
+    :param outpath:
+    :return:
+    """
+    angles = ["00", "45"] #选取角度
+    namelist = os.listdir(inpath)
+    train_files = []
+    test_files = []
+
+    for name in namelist:
+        namepath = os.path.join(inpath, name)
+        txtfiles = os.listdir(namepath)
+
+        for angle in angles:
+            filtered_file = [f for f in txtfiles if os.path.basename(f).startswith(angle)]
+            random.shuffle(filtered_file)
+            train_size = len(filtered_file) * 3 // 4
+            train_files.extend(filtered_file[:train_size])
+            test_files.extend(filtered_file[train_size:])
 
 
+        for f in train_files:
+            out_path_t = os.path.join(outpath, "train", name, f)
+            in_path = os.path.join(inpath, name, f)
+
+            os.makedirs(os.path.dirname(out_path_t),exist_ok=True)
+
+            with open(in_path, "r") as fr, open(out_path_t, "w") as fw:
+                fw.write(fr.read())
+
+        for f in test_files:
+            out_path_t = os.path.join(outpath, "test", name, f)
+            in_path = os.path.join(inpath, name, f)
+
+            os.makedirs(os.path.dirname(out_path_t),exist_ok=True)
+
+            with open(in_path, "r") as fr, open(out_path_t, "w") as fw:
+                fw.write(fr.read())
 
 
 
 if __name__ == '__main__':
-    rootpath = 'E:\Dataset\CASIA_B_2020'
-    dvd2(rootpath)
-
+    rootpath = "E:\Dataset\ev_CSISAA_sample4"
+    out = "E:\Dataset\ev_CSISAA_sample"
+    dvd3(rootpath,out)
